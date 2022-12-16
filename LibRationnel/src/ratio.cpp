729b1a0724b9fb5920482cdc39086e1f9e3bac58
DANLCARTON <ericthiberge@orange.fr>
2022-12-16 10:12:34 +0100

    operateurs

--- LibRationnel/src/ratio.cpp
+++ LibRationnel/src/ratio.cpp
@@ -15,29 +15,39 @@ Ratio convert_float_to_ratio(const T &real) {
     return 42;
 }
 
-Ratio operator+(const Ratio &rn) const {
-    //...
-    return 42;
+Ratio<T> operator+(const Ratio &rn) const {
+    Ratio ratio;
+    ratio.m_num = (this->m_num*rn.m_den)+(this->m_den*rn.m_num);
+    ratio.m_den = this->m_den*rn.m_den;
+    return ratio;
 }
 
 Ratio operator-(const Ratio &rn) const {
-    //...
-    return 42;
+    Ratio ratio;
+    ratio.m_num = (this->m_num*rn.m_den)-(this->m_den*rn.m_num);
+    ratio.m_den = this->m_den*rn.m_den;
+    return ratio;
 }
 
 Ratio operator*(const Ratio &rn) const {
-    //...
-    return 42;
+    Ratio ratio;
+    ratio.m_num = (this->m_num*rn.m_num);
+    ratio.m_den = (this->m_den*rn.m_den);
+    return ratio;
 }
 
 Ratio inverse() const {
-    //...
-    return 42;
+    Ratio ratio;
+    ratio.m_num = this->m_den;
+    ratio.m_den = this->m_num;
 } 
 
 Ratio operator/(const Ratio &rn) const {
-    //...
-    return 42;
+    Ratio ratio;
+    rn = rn.inverse();
+    ratio.m_num = (this->m_num*rn.m_num);
+    ratio.m_den = (this->m_den*rn.m_den);
+    return ratio;
 }
 
 std::ostream &operator<<(std::ostream &stream, const Ratio<T> &rn) {
