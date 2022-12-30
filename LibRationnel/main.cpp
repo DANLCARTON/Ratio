163b0413e9ff5e1b5c5fb0f9562dc43c738602f4
DANLCARTON <ericthiberge@orange.fr>
2022-12-30 16:11:01 +0100

    Merge branche maxime

--- LibRationnel/main.cpp
+++ LibRationnel/main.cpp
@@ -9,6 +9,8 @@ int main() {
     Ratio<int> e(3, 10);
     Ratio<int> f = e.sqrt();
     Ratio<int> g(-34, 10);
+    Ratio<int> h(34, -10);
+    Ratio<int> i(-34, -10);
     std::cout << a << std::endl;
     std::cout << b << std::endl;
     std::cout << c << std::endl;
@@ -52,6 +54,5 @@ int main() {
     std::cout << "//////////////" << std::endl;
     std::cout << convert_float_to_ratio(M_PI, 20) << std::endl;
     std::cout << convert_float_to_ratio(-M_PI, 20) << std::endl;
-    std::cout << convert_float_to_ratio(14852.958645, 20) << std::endl;   
-
+    std::cout << convert_float_to_ratio(14852.958645, 20) << std::endl;
 }  
